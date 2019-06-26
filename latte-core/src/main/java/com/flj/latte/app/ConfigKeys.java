9f0dd4d0840d6abcc068d034265bd9b345190fec
hotgalvanizing <steelhe@qq.com>
2019-06-27 00:25:45 +0800

    Initial commit

---
+++ latte-core/src/main/java/com/flj/latte/app/ConfigKeys.java
@@ -0,0 +1,23 @@
+package com.flj.latte.app;
+
+/**
+ * Created by 傅令杰 on 2017/3/29
+ */
+
+public enum ConfigKeys {
+    //配置域名
+    API_HOST,
+    //全局上下文
+    APPLICATION_CONTEXT,
+    //配置是否完成
+    CONFIG_READY,
+    ICON,
+    LOADER_DELAYED,
+    INTERCEPTOR,
+    WE_CHAT_APP_ID,
+    WE_CHAT_APP_SECRET,
+    ACTIVITY,
+    HANDLER,
+    JAVASCRIPT_INTERFACE,
+    WEB_HOST
+}
---
+++ latte-ec/src/main/java/com/flj/latte/ec/sign/SignUpDelegate.java
@@ -0,0 +1,137 @@
+package com.flj.latte.ec.sign;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
+import android.view.View;
+
+import com.diabin.latte.ec.R;
+import com.flj.latte.delegates.LatteDelegate;
+import com.flj.latte.net.RestClient;
+import com.flj.latte.net.callback.ISuccess;
+import com.flj.latte.util.log.LatteLogger;
+
+/**
+ * Created by 傅令杰 on 2017/4/22
+ */
+
+public class SignUpDelegate extends LatteDelegate {
+
+    private TextInputEditText mName = null;
+    private TextInputEditText mEmail = null;
+    private TextInputEditText mPhone = null;
+    private TextInputEditText mPassword = null;
+    private TextInputEditText mRePassword = null;
+
+    private ISignListener mISignListener = null;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (activity instanceof ISignListener) {
+            mISignListener = (ISignListener) activity;
+        }
+    }
+
+    private void onClickSignUp() {
+        if (checkForm()) {
+            RestClient.builder()
+                    .url("http://192.168.56.1:8080/RestDataServer/api/user_profile.php")
+                    .params("name", mName.getText().toString())
+                    .params("email", mEmail.getText().toString())
+                    .params("phone", mPhone.getText().toString())
+                    .params("password", mPassword.getText().toString())
+                    .success(new ISuccess() {
+                        @Override
+                        public void onSuccess(String response) {
+                            LatteLogger.json("USER_PROFILE", response);
+                            SignHandler.onSignUp(response, mISignListener);
+                        }
+                    })
+                    .build()
+                    .post();
+        }
+    }
+
+    private void onClickLink() {
+        getSupportDelegate().start(new SignInDelegate());
+    }
+
+    private boolean checkForm() {
+        final String name = mName.getText().toString();
+        final String email = mEmail.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String password = mPassword.getText().toString();
+        final String rePassword = mRePassword.getText().toString();
+
+        boolean isPass = true;
+
+        if (name.isEmpty()) {
+            mName.setError("请输入姓名");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误的邮箱格式");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        if (phone.isEmpty() || phone.length() != 11) {
+            mPhone.setError("手机号码错误");
+            isPass = false;
+        } else {
+            mPhone.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        if (rePassword.isEmpty() || rePassword.length() < 6 || !(rePassword.equals(password))) {
+            mRePassword.setError("密码验证错误");
+            isPass = false;
+        } else {
+            mRePassword.setError(null);
+        }
+
+        return isPass;
+    }
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_up;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+        mName = $(R.id.edit_sign_up_name);
+        mEmail = $(R.id.edit_sign_up_email);
+        mPhone = $(R.id.edit_sign_up_phone);
+        mPassword = $(R.id.edit_sign_up_password);
+        mRePassword = $(R.id.edit_sign_up_re_password);
+
+        $(R.id.btn_sign_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSignUp();
+            }
+        });
+
+        $(R.id.tv_link_sign_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickLink();
+            }
+        });
+    }
+}
